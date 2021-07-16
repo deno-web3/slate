19a1aa10f56f96f15129a63cbebfe5fa16ed4b33
v1rtl <pilll.pl22@gmail.com>
2021-07-17 02:18:45 +0300

    bump

--- mod.ts
+++ mod.ts
@@ -1,4 +1,4 @@
-import { contentType } from 'https://deno.land/x/media_types@v2.9.1/mod.ts'
+import { contentType } from 'https://deno.land/x/media_types@v2.9.2/mod.ts'
 declare namespace slate {
   export interface Collection {
     id: string
